32ca0a9bd1a66873a24826efe7a36baef6c7eb14
ibraheem anani <ibrah@DESKTOP-B08DFD8>
2024-08-09 13:38:26 +0300

    the Final update

--- routes/articles.js
+++ routes/articles.js
@@ -1,34 +1,50 @@
-const express = require('express');
-const router = express.Router();
-const Articale = require('./../models/article');
-const article = require('./../models/article');
+const express = require('express')
+const Article = require('./../models/article')
+const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new', {article: new Articale()})
+  res.render('articles/new', { article: new Article() })
 })
 
-router.get('/:id', async (req,res) => {
-  const article = await Articale.findById(req.params.id)
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('articles/edit', { article: article })
+})
+
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug })
   if (article == null) res.redirect('/')
-   res.render('articles/show'), {article:article}
+  res.render('articles/show', { article: article })
 })
 
-router.post('/', async (req,res) => {
-let article = new Articale({
-   title: req.body.title,
-   descriprtion: req.body.descriprtion,
-   markdown: req.body.markdown
+router.post('/', async (req, res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
 })
-try{
- article = await article.save() 
- res.redirect('/articles/${article.id}')
-}catch (e) {
-  console.log(e)
-  res.render('articles/new', {article:article})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
 }
-
-
-})
-
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,36 +1,23 @@
-const express = require("express")
+const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
-const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/mydatabase', {
-
-  })
+mongoose.connect('mongodb://localhost:27017/Blog')
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.use(express.urlencoded({extended: false}))
-
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test description 2'
-    }
-]
-    res.render('articles/index', { articles: articles})
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
 })
 
 app.use('/articles', articleRouter)
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+app.listen(5000)
